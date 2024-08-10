c615fcf6852b0c792b9d15b0f8d851d612303360
kok <jamesss075@gmail.com>
2024-08-10 09:59:08 +0300

    completed band-name-generator for day 1

---
+++ day1/band_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""A band name generator"""
+# concepts learnt
+# input function
+# variables
+# print()
+
+
+print('Welcome to the Band Name Generator')
+city_name = input("Whats name of the city you grew up in?\n")
+petname = input("Whats your pet nme?\n")
+
+print(f'Your band name could be {city_name} {petname}')
